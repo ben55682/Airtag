238f4baa14e862db8c77e2e37dfa5a517dc255f0
ben55682 <bengr2003@gmail.com>
2024-03-26 20:41:51 +0200

    Changes for test

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './comps/user/user.module';
 import { AirtagModule } from './comps/airtag/airtag.module';
 
+
 @Module({
   imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URL), UserModule,AirtagModule],
   controllers: [],
